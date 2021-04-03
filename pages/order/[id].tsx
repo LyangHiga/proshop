842386ae3f127e29b7fdb06da7c2b912dbb3558f
lyanghiga <lyang_higa@poli.ufrj.br>
2021-04-03 17:37:02 -0300

    Add Order get request

--- pages/order/[id].tsx
+++ pages/order/[id].tsx
@@ -1,5 +1,50 @@
-const OrderPage = () => {
-  return <div>Order Page</div>;
+import { GetServerSideProps } from "next";
+
+import Order from "../../models/Order";
+
+interface OrderProps {
+  order: Order;
+  err: boolean;
+}
+
+const OrderPage = ({ order, err }: OrderProps) => {
+  return <div>{order._id}</div>;
 };
 
 export default OrderPage;
+
+// why should I keep a global state here?
+// https://stackoverflow.com/questions/35328056/react-redux-should-all-component-states-be-kept-in-redux-store
+export const getServerSideProps: GetServerSideProps = async ({
+  params,
+  req,
+}) => {
+  const { id } = params!;
+  // TODO: Keep only token in cookie and remove usr from global state
+  const { token } = JSON.parse(req.cookies.user);
+  // check if user is logged in
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+
+  const data = await fetch(`http://localhost:5000/api/orders/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  });
+
+  const order = await data.json();
+
+  return {
+    props: {
+      order,
+      err: !data.ok,
+    },
+  };
+};
